42fe99d515d3bd7fa15ec57c0495224993c57ed4
Olga Polishchuk <olgapolichuk@ukr.net>
2023-04-07 00:04:22 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-import { Section } from './Section/Section';
-import { ContactsList } from './ContactsList/ContactsList';
-import { Filter } from './Filter/Filter';
-import { ToastContainer } from 'react-toastify';
+// import { Section } from './Section/Section';
+// import { ContactsList } from './ContactsList/ContactsList';
+// import { Filter } from './Filter/Filter';
+// import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Layout from './Layout';
 import Home from 'pages/Home';
